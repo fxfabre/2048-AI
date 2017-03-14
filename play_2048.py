be2c89e17488dc4fbb34ee19a684005874a915d2
fxfabre <fx.fabre@gmail.com>
2017-03-14 09:48:10 +0000

    Add eval strat

--- play_2048.py
+++ play_2048.py
@@ -3,8 +3,10 @@
 
 import os
 import time
+import math
 import logging
 import traceback
+import numpy as np
 
 from AI.ai_q_learning import Qlearning
 from GameGrids.LogGameGrid import GameGrid2048
@@ -19,12 +21,16 @@ class PlayGame:
 
     def Simulate(self):
         try:
-            nb_iter = 0
-            while nb_iter < 2000:
-                nb_iter += 1
-                self.playGame(nb_iter)
+            play_number = 0
+            while play_number < 2000:
+                play_number += 1
+                nb_moves, diff_update = self.playGame()
 
-                if nb_iter % 500000 == 0:
+                if play_number % 10 == 0:
+                    self._logger.info("{0:>3} End in {1:>3} iterations, diff = {2}".format(
+                        play_number, nb_moves, round(diff_update, 4)))
+
+                if play_number % 500000 == 0:
                     self._ai.SaveStates()
                 if self._logger.isEnabledFor(logging.DEBUG):
                     raise Exception("end game")
@@ -35,6 +41,31 @@ class PlayGame:
             traceback.print_tb(e.__traceback__)
             print("error :", e)
         self._ai.SaveStates()
+
+    def eval_strat(self):
+        nb_moves_done = np.zeros(100)
+        play_number = 0
+        nb_total_play = 100
+        while play_number < nb_total_play:
+            play_number += 1
+            nb_moves, _ = self.playGame()
+            nb_moves_done[nb_moves] += 1
+
+            if play_number % 10 == 0:
+                print("Playing game ", play_number)
+
+        moyenne = 0
+        moyenne_square = 0
+        for i in range(nb_moves_done.shape[0]):
+            nb_moves = nb_moves_done[i]
+            if nb_moves > 0:
+                moyenne += i * nb_moves
+                moyenne_square += i * i * nb_moves
+                print(i, nb_moves)
+        moyenne /= nb_total_play
+        moyenne_square /= nb_total_play
+        print("mean : ", moyenne)
+        print("std  : ", math.sqrt(moyenne_square - moyenne * moyenne))
 
     def playGame(self, play_number=0):
         current_grid = GameGrid2048(nb_rows=constants.NB_ROWS, nb_columns=constants.NB_COLS)
@@ -72,9 +103,7 @@ class PlayGame:
                 diff_update += self._ai.RecordState(old_state, current_state, move_dir)
                 is_game_over = current_grid.is_game_over()
 
-        if play_number % 10 == 0:
-            self._logger.info("{0:>3} End in {1:>3} iterations, diff = {2}".format(
-                play_number, nb_iter, round(diff_update, 4)))
+        return nb_iter, diff_update
 
     def init_logger(self):
         log_format = '%(asctime)-15s %(message)s'
@@ -84,4 +113,5 @@ class PlayGame:
 
 if __name__ == '__main__':
     game = PlayGame()
-    game.Simulate()
+    # game.Simulate()
+    game.eval_strat()
